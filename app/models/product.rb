a7b4a9557de521dbc8b6562658a2fd4176e8108a
Eliza Plowden <elizaplowden@gmail.com>
2021-03-20 15:31:20 +0000

    added product and category to index and show. Changed associations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  has_many :listing_deals, dependent: :destroy
+  # has_many :listing_deals, dependent: :destroy
   has_many :bids, dependent: :destroy
 
   validates :name, presence: true
