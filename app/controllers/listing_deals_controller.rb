f2149cd0627d4b715073eae24e989c5b02f86fb1
Eliza Plowden <elizaplowden@gmail.com>
2021-03-20 12:57:08 +0000

    added create listing deal method

--- app/controllers/listing_deals_controller.rb
+++ app/controllers/listing_deals_controller.rb
@@ -13,12 +13,26 @@ class ListingDealsController < ApplicationController
     @listing_deal = ListingDeal.new
   end
 
+  def create
+    @listing_deal = ListingDeal.new(listing_deal_params)
+    @listing_deal.user = current_user
+    if @listing_deal.save
+      redirect_to listing_deal_path(@listing_deal), alert: "Deal listed successfully"
+    else
+      render :new
+  end
+
+
 
 
   private
 
   def find_listing_deal
     @listing_deal = ListingDeal.find(params[:id])
+  end
+
+  def listing_deal_params
+    params.require(:listing_deal).permit(:product_id, :user_id, :min_amount, :other_requirements, :time_period, :volume, :completed_at)
   end
 
 end
