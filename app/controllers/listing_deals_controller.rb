a015381dd71094a5f760c03d3b07d75f4d130605
Eliza Plowden <elizaplowden@gmail.com>
2021-03-20 12:33:52 +0000

    added show method and view with basic functionality. Need to add different user actions later

--- app/controllers/listing_deals_controller.rb
+++ app/controllers/listing_deals_controller.rb
@@ -1,5 +1,19 @@
 class ListingDealsController < ApplicationController
+  before_action :find_listing_deal, only: [:show, :edit, :destroy]
+
   def index
     @listing_deals = ListingDeal.all
   end
+
+  def show
+    @bids = @listing_deal.bids
+  end
+
+
+  private
+
+  def find_listing_deal
+    @listing_deal = ListingDeal.find(params[:id])
+  end
+
 end
